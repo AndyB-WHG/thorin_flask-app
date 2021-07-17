c107240057e1efd034e8a652233991b021e53f1e
AndyB-WHG <andrew.bond2@gmail.com>
2021-07-17 09:38:53 +0000

    feat: added links to each company member. Clicking on link open a new page using both the base template and data from the company.json file by comparing the member url/name with the json file contents.

--- run.py
+++ run.py
@@ -17,6 +17,17 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
+@app.route("/about/<member_name>")      # create a new page with the URL https://............/about/thorin  (for example)   The <member_name> is pulled from the link in
+#                                         the About page)
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html/", member=member)
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
