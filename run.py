60b9b8da0b74a2c836211d8e2805aeee29b526bc
AndyB-WHG <andrew.bond2@gmail.com>
2021-07-20 15:58:01 +0000

    fix: added flash messages.

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, flash
 
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -18,8 +21,10 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/about/<member_name>")      # create a new page with the URL https://............/about/thorin  (for example)   The <member_name> is pulled from the link in
-#                                         the About page)
+@app.route("/about/<member_name>")      # create a new page with the URL
+# https://............/about/thorin  (for example)   The <member_name> is
+#  pulled from the link in
+#  the About page)
 def about_member(member_name):
     member = {}
     with open("data/company.json", "r") as json_data:
@@ -33,13 +38,22 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
+        # return all form inputs
         print(request.form)
+        # or... returns 'none' if "name" input doesn't exist for example
+        print(request.form.get("name"))
+        # or... returns an 'exception' if the "email" input doesn't
+        # exist for example
+        print(request.form["email"])
+        flash("Thanks {}, we have received your message.".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
     return render_template("careers.html", page_title="Careers")
+
 
 if __name__ == "__main__":
     app.run(
